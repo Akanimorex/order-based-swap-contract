911922efc2bc55a45a55fee3f82dc5eecc6e9bc1
Akanimorex <akanimoime2016@gmail.com>
2024-09-19 14:51:17 +0100

    initial commit

---
+++ contracts/StakeEth.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+
+contract TokenSwap {
+    struct Order {
+        address depositor;
+        address tokenOffered;
+        uint256 amountOffered;
+        address tokenRequested;
+        uint256 amountRequested;
+        bool isActive;
+    }
+
+    Order[] public orders;
+
+    event OrderCreated(uint256 orderId, address indexed depositor, address tokenOffered, uint256 amountOffered, address tokenRequested, uint256 amountRequested);
+    event OrderFilled(uint256 orderId, address indexed buyer);
+
+    function createOrder(address _tokenOffered, uint256 _amountOffered, address _tokenRequested, uint256 _amountRequested) external {
+        require(_amountOffered > 0, "Amount offered must be greater than zero");
+        require(_amountRequested > 0, "Amount requested must be greater than zero");
+
+        IERC20(_tokenOffered).transferFrom(msg.sender, address(this), _amountOffered);
+
+        Order memory newOrder;
+
+        newOrder.depositor = msg.sender;
+        newOrder.tokenOffered = _tokenOffered;
+        newOrder.amountOffered = _amountOffered;
+        newOrder.tokenRequested = _tokenRequested;
+        newOrder.amountRequested = _amountRequested;
+        newOrder.isActive = true;
+
+        orders.push(newOrder);
+
+      
+
+        emit OrderCreated(orders.length - 1, msg.sender, _tokenOffered, _amountOffered, _tokenRequested, _amountRequested);
+    }
+
+    function fillOrder(uint256 _orderId) external {
+        Order storage order = orders[_orderId];
+        require(order.isActive, "Order is not active");
+
+        IERC20(order.tokenRequested).transferFrom(msg.sender, order.depositor, order.amountRequested);
+        IERC20(order.tokenOffered).transfer(msg.sender, order.amountOffered);
+
+        order.isActive = false;
+
+        emit OrderFilled(_orderId, msg.sender);
+    }
+}
+
+
+
---
+++ test/Lock.ts
@@ -0,0 +1,101 @@
+import { loadFixture,} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre, { ethers } from "hardhat";
+
+describe("StakeEth", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployContractAndSetVariables() {
+    const StakeEth = await ethers.getContractFactory("StakeEth");
+    const contractBalance = ethers.parseEther("100.0");
+    // const contractBalance = ethers.parseUnits("100.0", 10);
+
+    const stakeEth = await StakeEth.deploy({value: contractBalance});
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+
+    return { stakeEth, owner };
+  }
+
+  describe("Deployment", function () {
+    it("Should compare that both address is the same", async function () {
+      const { stakeEth, owner} = await loadFixture(deployContractAndSetVariables);
+      expect(await  stakeEth.owner()).to.equal(await owner.getAddress());
+      
+      // expect( await stakeEth.).to.above(0);
+      expect( await stakeEth.owner.);
+
+    });
+
+
+    
+  });
+
+  // describe("Withdrawals", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert with the right error if called too soon", async function () {
+  //       const { lock } = await loadFixture(deployOneYearLockFixture);
+
+  //       await expect(lock.withdraw()).to.be.revertedWith(
+  //         "You can't withdraw yet"
+  //       );
+  //     });
+
+  //     it("Should revert with the right error if called from another account", async function () {
+  //       const { lock, unlockTime, otherAccount } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       // We can increase the time in Hardhat Network
+  //       await time.increaseTo(unlockTime);
+
+  //       // We use lock.connect() to send a transaction from another account
+  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+  //         "You aren't the owner"
+  //       );
+  //     });
+
+  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+  //       const { lock, unlockTime } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       // Transactions are sent using the first signer by default
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).not.to.be.reverted;
+  //     });
+  //   });
+
+  //   describe("Events", function () {
+  //     it("Should emit an event on withdrawals", async function () {
+  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw())
+  //         .to.emit(lock, "Withdrawal")
+  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+  //     });
+  //   });
+
+  //   describe("Transfers", function () {
+  //     it("Should transfer the funds to the owner", async function () {
+  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).to.changeEtherBalances(
+  //         [owner, lock],
+  //         [lockedAmount, -lockedAmount]
+  //       );
+  //     });
+  //   });
+  // });
+});
